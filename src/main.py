0beff0ae8b3c739ce43ee6d03dcd4186b805e079
Devashish-Pisal <dpisal85@gmail.com>
2026-05-17 19:06:47 +0200

    plot all graphs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import numpy as np
 from types import SimpleNamespace
 from configs.csv_file_config import COLUMN_NAMES
+from configs.path_config import RAW_DATA_DIR
 from src.plotting import *
-from src.data_processing import get_imagenet_data, get_fornet_data, load_raw_data
+from src.data_processing import get_imagenet_data, get_fornet_data, load_raw_file
 
 
 if __name__ == '__main__':
     csv_config = SimpleNamespace(**COLUMN_NAMES)
 
-    result = load_raw_data()
+    output_file_path = RAW_DATA_DIR / "steps_scaling_exponent-1.0_output.csv"
+    scaling_exponent = 1.0
+
+    result = load_raw_file(output_file_path)
     imgnet_data = get_imagenet_data(result[0],csv_config)
     fornet_data = get_fornet_data(result[0], csv_config)
     ds_samples = imgnet_data[csv_config.real_ds_samples]  # fornet_data.real_ds_samples also works (both are same)
@@ -27,18 +31,21 @@ if __name__ == '__main__':
     fornet_top_5_acc = fornet_data[csv_config.max_val_acc5]
 
     set_style()
-    # plot_scaling_law(ds_samples, imgnet_val_loss, "ImageNet")
-    # plot_scaling_law(ds_samples, fornet_val_loss, "ForNet")
-    # plot_dataset_size_scaling_comparison(ds_samples, imgnet_val_loss, fornet_val_loss)
-    # plot_acc_comparison(ds_samples, imgnet_top_1_acc, fornet_top_1_acc, "Top-1")
-    # plot_acc_comparison(ds_samples, imgnet_top_5_acc, fornet_top_5_acc, "Top-5")
-    # plot_steps_allocation(ds_samples, imgnet_steps, "ImageNet")
-    # plot_steps_allocation(ds_samples, fornet_steps, "ForNet")
-    # plot_flops_scaling_comparison(flops, imgnet_val_loss, fornet_val_loss)
-    # plot_compute_efficiency_comparison(flops, imgnet_top_1_acc, fornet_top_1_acc, "Top-1")
-    # plot_compute_efficiency_comparison(flops, imgnet_top_5_acc, fornet_top_5_acc, "Top-5")
-    # plot_sample_efficiency_comparison(ds_samples, imgnet_top_1_acc, fornet_top_1_acc, "Top-1", 0.70)
-    # plot_sample_efficiency_comparison(ds_samples, imgnet_top_5_acc, fornet_top_5_acc, "Top-5", 0.90)
+    plot_steps_allocation(ds_samples, imgnet_steps, "ImageNet", scaling_exponent)
+    plot_steps_allocation(ds_samples, fornet_steps, "ForNet", scaling_exponent)
+    plot_scaling_law(ds_samples, imgnet_val_loss, "ImageNet")
+    plot_scaling_law(ds_samples, fornet_val_loss, "ForNet")
+    plot_dataset_size_scaling_comparison(ds_samples, imgnet_val_loss, fornet_val_loss)
+    plot_flops_scaling_comparison(flops, imgnet_val_loss, fornet_val_loss)
+
+    plot_acc_comparison(ds_samples, imgnet_top_1_acc, fornet_top_1_acc, "Top-1")
+    plot_acc_comparison(ds_samples, imgnet_top_5_acc, fornet_top_5_acc, "Top-5")
+
+    plot_compute_efficiency_comparison(flops, imgnet_top_1_acc, fornet_top_1_acc, "Top-1")
+    plot_compute_efficiency_comparison(flops, imgnet_top_5_acc, fornet_top_5_acc, "Top-5")
+
+    plot_sample_efficiency_comparison(ds_samples, imgnet_top_1_acc, fornet_top_1_acc, "Top-1", 0.70)
+    plot_sample_efficiency_comparison(ds_samples, imgnet_top_5_acc, fornet_top_5_acc, "Top-5", 0.90)
 
 
 
