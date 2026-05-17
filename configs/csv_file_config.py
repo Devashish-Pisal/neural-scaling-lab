fcb767674d0bbfc4ad9862d7cf6cdd422272e800
Devashish-Pisal <dpisal85@gmail.com>
2026-05-17 19:02:46 +0200

    include min, max in column names

---
+++ configs/csv_file_config.py
@@ -0,0 +1,19 @@
+
+
+COLUMN_NAMES = {
+    "model_name": "Model",
+    "patch_size": "Patch Size",
+    "ds_name": "Dataset Name",
+    "fg_range": "FG Range",
+    "bg_range": "BG Range",
+    "ds_size_fraction": "Dataset Fraction",
+    "calc_ds_samples": "Calculated DS Samples",
+    "calc_steps": "Calculated Steps",
+    "calc_epochs": "Epochs",
+    "real_ds_samples": "Real DS Samples",
+    "real_steps": "Real Steps",
+    "real_FLOPs": "FLOPs",
+    "min_val_loss": "Val/Loss (Min)",
+    "max_val_acc1": "Val/Acc1 (Max)",
+    "max_val_acc5": "Val/Acc5 (Max)",
+}
