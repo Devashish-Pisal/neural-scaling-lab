4bbff2825b798447cab52e8e4432816691ca0404
Devashish-Pisal <dpisal85@gmail.com>
2026-05-17 19:07:56 +0200

    add dedicated config for plots

---
+++ configs/plot_config.py
@@ -0,0 +1,12 @@
+
+PLOT_CONFIG = {
+    "plot_style": "seaborn-v0_8-whitegrid",
+    "figure_size": (6,5),
+    "font_size": 12,
+    "title_size": 14,
+    "label_size": 13,
+    "legend_font_size": 11,
+    "dpi": 100,
+    "output_dpi": 300,
+    "save_format": "png", # either png or pdf
+}
