b0fc34a3d94005647526e1504cdebc34d430b500
Devashish-Pisal <dpisal85@gmail.com>
2026-06-02 19:29:06 +0200

    implement database filter function

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pathlib import Path
-from configs.path_config import RAW_DATA_DIR
+from configs.path_config import RAW_DATA_DIR, MAIN_DATABASE_PATH
 
 
 def load_raw_file(file_name:Path):
@@ -23,3 +23,26 @@ def get_imagenet_data(processed_data, csv_config):
 def get_fornet_data(processed_data, csv_config):
     processed_data = processed_data[processed_data[csv_config.ds_name] == "ForNet"]
     return processed_data
+
+
+def filter_database(
+        run_id=None, run_name=None, model_name=None, train_dataset_name=None,
+        train_dataset_fraction=None, fg_range=None, bg_range=None, total_epochs=None):
+    database = pd.read_csv(MAIN_DATABASE_PATH)
+    if run_id is not None:
+        database = database[database["run_id"] == run_id]
+    if run_name is not None:
+        database = database[database["run_name"] == run_name]
+    if model_name is not None:
+        database = database[database["model_name"] == model_name]
+    if train_dataset_name is not None:
+        database = database[database["train_dataset_name"] == train_dataset_name]
+    if train_dataset_fraction is not None:
+        database = database[database["train_dataset_fraction"] == train_dataset_fraction]
+    if fg_range is not None:
+        database = database[database["fg_range"] == fg_range]
+    if bg_range is not None:
+        database = database[database["bg_range"] == bg_range]
+    if total_epochs is not None:
+        database = database[database["total_epochs"] == total_epochs]
+    return database
