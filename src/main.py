32ba12a19d1d306dcb31a31a3f623e4a67a24a2f
Devashish-Pisal <dpisal85@gmail.com>
2026-08-04 14:40:21 +0200

    plot pareto graph

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 from types import SimpleNamespace
+
+import pandas as pd
+from src.plot_pareto import plot_pareto_frontier
 from configs.config import COLUMN_NAMES
 from configs.path_config import RAW_DATA_DIR
 from src.plotting import *
@@ -93,18 +96,23 @@ if __name__ == '__main__':
         "ForNet"
     )
    '''
-    imgnet_run = filter_database(train_dataset_name="fornet/all/1.0")
-    fornet_run = filter_database(train_dataset_name="fornet/all/cos")
-    plot_scaling_law(imgnet_run)
-    plot_scaling_law(fornet_run)
+    # imgnet_run = filter_database(train_dataset_name="fornet/all/1.0", model_name="ViT-S/16")
+    # fornet_run = filter_database(train_dataset_name="fornet/all/cos", model_name="ViT-S/16", bg_range="0-100")
+
+    # plot_scaling_law(imgnet_run)
+    # plot_scaling_law(fornet_run)
 
      
-    plot_dataset_size_scaling_comparison(filter_database())
-    plot_fg_bg_heatmaps(filter_database())
-    plot_fornet_vs_imagenet_delta_gain(filter_database())
+    # plot_dataset_size_scaling_comparison(filter_database())
+    # plot_fg_bg_heatmaps(filter_database())
+    # plot_fornet_vs_imagenet_delta_gain(filter_database())
 
-    plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_loss")
-    plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_acc1")
-    plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_acc5")
+    # plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_loss")
+    # plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_acc1")
+    # plot_crossover_epoch_vs_dataset_fraction(filter_database(), "val_acc5")
 
+
+    # plot_flops_scaling_comparison(filter_database())
+    # plot_model_scaling_comparison(filter_database())
+    plot_pareto_frontier(filter_database())
     # plot_crossover_flops_scaling(filter_database())
